f4dfa04e31af8ab7d5fea665b9805e4199201ba6
Leturgone <finekon221@gmail.com>
2024-04-11 23:57:03 +0300

    Убраны тудушки

--- app/src/main/java/com/example/cursework2_2/MoviesListFragment.java
+++ app/src/main/java/com/example/cursework2_2/MoviesListFragment.java
@@ -8,15 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MoviesListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class MoviesListFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+public class MoviesListFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -28,15 +21,6 @@ public class MoviesListFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MoviesListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MoviesListFragment newInstance(String param1, String param2) {
         MoviesListFragment fragment = new MoviesListFragment();
         Bundle args = new Bundle();
