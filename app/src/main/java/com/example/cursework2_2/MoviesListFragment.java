55cc0df709ffd9870a7862f10932da8eafc69cd5
Leturgone <finekon221@gmail.com>
2024-04-23 22:29:43 +0300

    Сделано отображение списка фильмов

--- app/src/main/java/com/example/cursework2_2/MoviesListFragment.java
+++ app/src/main/java/com/example/cursework2_2/MoviesListFragment.java
@@ -2,20 +2,24 @@ package com.example.cursework2_2;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 
 public class MoviesListFragment extends Fragment {
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    MyDatabaseHelper myDB;
+    RecyclerView movieList;
+    List<Movie> movies;
 
     public MoviesListFragment() {
         // Required empty public constructor
@@ -24,8 +28,6 @@ public class MoviesListFragment extends Fragment {
     public static MoviesListFragment newInstance(String param1, String param2) {
         MoviesListFragment fragment = new MoviesListFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -33,16 +35,22 @@ public class MoviesListFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_movies_list, container, false);
     }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        movieList = view.findViewById(R.id.recyclerView);
+        myDB = new MyDatabaseHelper(getActivity());
+        movies = myDB.getAllMovies();
+        MovieAdapter adapter = new MovieAdapter(movies);
+        movieList.setLayoutManager(new LinearLayoutManager(getActivity()));
+        movieList.setAdapter(adapter);
+    }
+
 }
